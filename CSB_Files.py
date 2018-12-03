ea008d7ff094511f48c77b4a8d2a573ea9eda76f
linxiancheng <linxiancheng@stevegame.cn>
2018-12-03 18:18:07 +0800

    generate file dict

--- CSB_Files.py
+++ CSB_Files.py
@@ -5,20 +5,6 @@ import os
 import json
 import hashlib
 
-class CSB_Files:
-    def __init__(self,path):
-        __Path = path
-    __Count = 0 #统计文件数量 两个下划线的为
-# 单下划线是protected变量，双下划线是私有变量，私有变量 在外部可以通过self.__Count 进行访问
-    publicCount = 0 # 类的公开变量
-    def count(self):
-        print "1111"
-
-# csbfiles = CSB_Files("aaa")
-# print "Employee.__doc__:", csbfiles.__doc__
-# print "Employee.__module__:", csbfiles.__module__
-# print "Employee.__dict__:", csbfiles.__dict__
-
 # 读取行内容
 # txt = open("test.txt")
 # line = txt.readlines()
@@ -37,11 +23,12 @@ def initPathFiles(filepath , list):
     if os.path.isdir(filepath):
         for ccfile in os.listdir(filepath):
             if os.path.isdir(filepath + "/" + ccfile):
-                print(filepath + "/" + ccfile + "----- 3")
+                # print(filepath + "/" + ccfile + "----- 3")
                 initPathFiles(filepath + "/" + ccfile , list)
             else:
-                print(filepath + "/" + ccfile + "-----2")
-                list.append(filepath + "/" + ccfile + "\n")
+                # print(filepath + "/" + ccfile + "-----2")
+                # list.append(filepath + "/" + ccfile + "\n") # 写入到列表中，不需要加换行符，从列表中写入文件才需要加入换行符
+                list.append(filepath + "/" + ccfile ) # 写入到列表中，不需要加换行符，从列表中写入文件才需要加入换行符
     else:
         print(filepath + "-----1")
         list.append(filepath + "\n")
@@ -57,40 +44,69 @@ def Test2(rootDir):
 #读取路径下的所有资源文件分类后写入到文件中
 class gameres:
     file = open("files.txt" , "w+")
-    # def __init__(self):
+    file.close()
     fileList = []
     initPathFiles(filepath , fileList)
-
-    filedict = {}
-    for pathbylist in fileList:
-        print(pathbylist)
-        abspath = pathbylist
-        if not os.path.isabs(abspath):
-            abspath = os.path.abspath(pathbylist)
-        if os.path.isabs(abspath):                  #可以直接输出
-            print("2222222222222222222")
-        print(abspath)
-        # print abspath.replace("\\" , "/")   # 不管用
-        # abspath = repr(abspath)[1:-1]     # repr 不行
-        # abspath = eval("""%s""" % str(abspath))   # eval(r"%s" % abspath) 不行，r+abspath不行
-        print(os.path.exists(abspath))
-        print(os.path.isfile(abspath))
-        # print(os.path.exists(r"D:\Python_FileDispose\res\111.txt"))
-        # print(os.path.exists(r"D:\Python_FileDispose\res\fqqq\222.png"))
-        # print(os.path.exists(r"D:\Python_FileDispose\res\fccc\333.txt"))
-
-        if os.path.exists(abspath) and os.path.isfile(abspath):
-            singlepath  = os.path.splitext(abspath)
-            print(singlepath)
-
-
-    # file.write("".join(fileList))
-    file.close()
-
+    # file.write("".join(fileList))   #应该遍历后逐行逐行去写，而不是一次性写入到文件中，换行符在写入文件的时候加载行尾
     # jsonfile = open(jsonfilename   , "r+")
     # jsondict = json.load(jsonfile)
     # print json.dumps( jsondict , ensure_ascii=False ,  encoding= "utf -8" , indent=4)
 
+    filedict = {}
+    def initFileDict(self):
+        def fillDict(typedict , filepath):
+            pathdict = {}
+            typedict[filepath] = pathdict
+            pathdict["md5"] = self.getFileMd5(filepath)
+            # pathdict["md5"] = self.getFileMd5(filepath)
+            pathdict["size"] = os.path.getsize(filepath)
+            #len(dict)   #返回dict元素个数
 
-    # if isinstance(jsondict , dict):
-        # print( jsondict.get("fileNameData", "666"))   #遍历所有的容器，找到key值为fileNameData的地方
+        for pathbylist in self.fileList:
+            abspath = pathbylist
+            if not os.path.isabs(abspath):
+                abspath = os.path.abspath(pathbylist)
+            singlepath , filetype = os.path.splitext(abspath)
+            if filetype in self.filedict:
+                typedict = self.filedict.get(filetype)
+                fillDict(typedict , abspath)
+            else:
+                typedict = {}
+                self.filedict[filetype] = typedict
+                fillDict(typedict, abspath)
+
+    def getFileMd5( self,path ):
+        if os.path.isfile(path):
+            filesize = os.path.getsize(path)
+            if filesize > 1024 * 10:   # 大文件获取md5值的方法
+                return self.bigFileMd5(path)
+            else:
+                return self.smallFileMd5(path)
+        else:
+            print(" path error : " + path)
+
+    def bigFileMd5(self , path):
+        md5_obj = hashlib.md5()
+        f_stream = open(path, "rb")
+        while True:
+            cut_stream = f_stream.read(8069)
+            if not cut_stream:
+                break
+            md5_obj.update(cut_stream)
+        hask_code = md5_obj.hexdigest()
+        f_stream.close()
+        md5_code = str(hask_code).lower()
+        return md5_code
+
+    def smallFileMd5(self , path):
+        md5_obj = hashlib.md5()
+        f_stream = open(path , "rb")
+        md5_obj.update(f_stream.read())
+        hash_code = md5_obj.hexdigest()
+        f_stream.close()
+        md5_code = str(hash_code).lower()
+        return md5_code
+
+t = gameres()
+t.initFileDict()
+print(json.dumps(t.filedict, ensure_ascii=False, encoding="utf -8", indent=4))
