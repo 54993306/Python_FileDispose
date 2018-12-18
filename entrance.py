fa4529f9a0d47f0bb1cb8d96ec867ba0bd5e9b61
linxiancheng <linxiancheng@stevegame.cn>
2018-12-18 12:00:09 +0800

    excute texture package command package big png succeed

--- entrance.py
+++ entrance.py
@@ -17,19 +17,19 @@ import packageImage
 if not TEST:
     referenceRes = {}    #文件引用计数表
 
-    t = totalResDict.totalRes() #初始化所有的资源信息
-    t.initFileDict()
+    # t = totalResDict.totalRes() #初始化所有的资源信息
+    # t.initFileDict()
 
-    jc= jsonFileRes.jsonRes(t.filedict)  # 初始化所有json中包含的资源信息
-    jc.initRecordFile()
+    # jc= jsonFileRes.jsonRes(t.filedict)  # 初始化所有json中包含的资源信息
+    # jc.initRecordFile()
 
     # 初始化代码中包含的资源信息
     # cre = codeRes.codeRes(t.filedict)
     # cre.initResList()
 
     # 小图合并大图
-    # pcg = packageImage.packageImage(t.filedict)
-    # pcg.sortReference()
+    pcg = packageImage.packageImage()
+    pcg.sortReference()
 
     # 修改json文件为使用大图
     # cg = fileChange.replaceImage()
--- packageImage.py
+++ packageImage.py
@@ -9,12 +9,8 @@ import json
 from PIL import Image
 
 class packageImage:
-    def __init__(self , pResDict):
-        if not pResDict:
-            assert(False)
-        self.ResDict = pResDict
     packageList = {}
-    sortRefList = {}
+    sortRefList = {}  # key:path , value:reference
 
     def sortReference(self):
         ref_stream = open(comFun.ReferenceFIle,"r")
@@ -24,11 +20,42 @@ class packageImage:
                 self.sortRefList[dic1["FilePath"]] = len(dic1["RefList"])
             else:
                 print "refresh is : " + str(len(dic1["RefList"])) + "path: " + dic1["FilePath"]
-        self.sortRefList = sorted(self.sortRefList.items(), key=lambda refNum: refNum[1] , reverse = True)
-        print json.dumps(self.sortRefList, ensure_ascii=False, encoding="utf-8", indent=4)
+        # self.sortRefList = sorted(self.sortRefList.items(), key=lambda refNum: refNum[1] , reverse = True)
+        # print json.dumps(self.sortRefList, ensure_ascii=False, encoding="utf-8", indent=4)
+        self.countPackagImage()
 
     def countPackagImage(self):
-        print 1
+        # shutil.move(src, dst)# 移动文件或重命名
+        TEXTURE_PACK_PATH = r"C:\Program Files\CodeAndWeb\TexturePacker\bin"
+        PACKAGE_TYPE = r"--multipack"
+        PLIST_PATH = r"D:\Python_FileDispose\res\out{n}.plist"
+        PNG_PATH = r"D:\Python_FileDispose\res\out{n}.png"
+        PNG_MAX_SIZE = 1024  # 输出的图片大小
+        SOURCE_FOLDER = r"D:\Python_FileDispose\real_res\111"
+        fileStr = ""
+        for tPath , referenceNum in self.sortRefList.iteritems():
+            if referenceNum > 2:
+                _,filetype = os.path.splitext(tPath)
+                if cmp(filetype , ".png") != 0:
+                    print tPath
+                    continue
+                # 根据md5值，找到相应的新路径的位置，打包大图使用新路径的打包大图，新路径的图已经修改过名字
+
+                # print Image.open(tPath).size
+                if max(Image.open(tPath).size) >= PNG_MAX_SIZE:
+                    print "max size path : " + tPath
+                else:
+                    fileStr = fileStr + tPath + " "
+        SOURCE_FOLDER = fileStr
+        PACKAGE_COMMOND = "TexturePacker.exe %s --data %s --sheet %s --max-size %d %s" % \
+                          (PACKAGE_TYPE, PLIST_PATH, PNG_PATH, PNG_MAX_SIZE, SOURCE_FOLDER)
+        # print fileStr
+        os.chdir(TEXTURE_PACK_PATH)
+        os.system(PACKAGE_COMMOND)
+        # os.system(r"cd C:\Program Files\CodeAndWeb\TexturePacker\bin && dir")
+        # print TEXTURE_PACK_PATH + "&&" + PACKAGE_COMMOND
+        # os.system("ipconfig")
+        # os.system("cmd ;%s ;%s" % (TEXTURE_PACK_PATH , "TexturePacker.exe"))
         # 通过分析打印数据，当引用计数大于2时，可以作为第一批的加载数据处理。
 
         # print len(refDict)
