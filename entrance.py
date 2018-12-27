6fc34cea8801eff2e6c767b12cb866f6bc8f64c1
linxiancheng <linxiancheng@stevegame.cn>
2018-12-27 20:06:41 +0800

    python excute Linux command in windows system

--- entrance.py
+++ entrance.py
@@ -23,8 +23,8 @@ if not TEST:
     # jc.initRecordFile()
 
     # 初始化代码中包含的资源信息
-    cre = codeRes.codeRes()
-    cre.initResList()
+    # cre = codeRes.codeRes()
+    # cre.initResList()
 
     # 小图合并大图
     # pcg = packageImage.packageImage()
@@ -37,26 +37,31 @@ else:
     pathstr = "./oldJson/code.lua"
     str_stream = open(pathstr , "r")
     resList = []
+    def replacePattern(match):
+        if not match:
+            return ""
+        print "replacePattern : " + match.group("Pattern")
+        return "ccc.png"  # 返回的内容用来替换被匹配的内容
+
     for lineNum, line in enumerate(str_stream):
-        pattern = re.compile(r"[\"](?P<pattern>[^:\"]*" + r".png" + r")[\"]")  # 找到包含资源的行，所有的资源都会被修改路径，统一进行管理
+        pattern = re.compile(r"[\"](?P<Pattern>[^:\"]*" + r".png" + r")[\"]")  # 找到包含资源的行，所有的资源都会被修改路径，统一进行管理
         # serchObj = pattern.sub(r'<---->\1</---->' , line)  # \1...\9	匹配第n个分组的内容。
+        print "before : " + line
+        print "after : " + pattern.sub(replacePattern , line)
         # print serchObj
-        print pattern.search(line).group("pattern")  # 对于一行中，包含多个类型的情况是否有相应的考虑
+        # print pattern.search(line).group("Pattern")  # 对于一行中，包含多个类型的情况是否有相应的考虑
         # serchList = pattern.findall(line)  # 对于一行中，包含多个类型的情况
         # resList.extend(serchList)
-    print resList
-
-    bold = re.compile(r'\*{2}(.*?)\*{2}')
-    text = 'Make this **cai**.  This **junsheng**.'
-    # print('Text:', text)
-    # print('Bold:', bold.sub(r'<b>\1</b>', text))
-
-    line = "Cats are smarter than dogs"
-    matchObj = re.match(r'(.*) are (.*?) .*', line, re.M | re.I)
-    if matchObj:
-        print "matchObj.group() : ", matchObj.group()
-        print "matchObj.group(1) : ", matchObj.group(1)
-        print "matchObj.group(2) : ", matchObj.group(2)
-    else:
-        print "No match!!"
-
+    # os.system("cmd && wsl && sed")
+    oldpath = os.getcwd()
+    os.chdir("D:\Python_FileDispose")
+    print os.getcwd()
+    TEXTURE_PACK_PATH = r"C:\Program Files (x86)\TexturePacker\bin"
+    # os.system("bash;sed -i 's/new/ccc/g' ./file.txt")
+    # os.popen("sed -i 's/new/ccc/g' ./file.txt")
+    # os.chdir(TEXTURE_PACK_PATH)
+    # os.system("TexturePacker.exe")
+    # os.system("wsl sed -i 's/ccc/new/g' ./file.txt")
+    os.system("wsl sed -i 's/new/ccc/g' ./file.txt")
+    os.chdir(oldpath)
+    print os.getcwd()
