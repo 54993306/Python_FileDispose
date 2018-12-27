36aadda249c20e28ab4bc6a14eef7a57d075688f
linxiancheng <linxiancheng@stevegame.cn>
2018-12-27 10:44:54 +0800

    match one line multi pattern and replace match content

--- entrance.py
+++ entrance.py
@@ -1,7 +1,7 @@
 
 # -*- coding: UTF-8 -*-
-TEST = False
-# TEST = True
+# TEST = False
+TEST = True
 
 import os
 import json
@@ -39,8 +39,15 @@ else:
     resList = []
     for lineNum, line in enumerate(str_stream):
         pattern = re.compile(r"[\"]([^:\"]*" + r".png" + r")[\"]")  # 找到包含资源的行，所有的资源都会被修改路径，统一进行管理
-        serchObj = pattern.findall(line)  # 对于一行中，包含多个类型的情况是否有相应的考虑
-        resList.extend(serchObj)
-    print resList
-        # print serchObj.groups()
+        serchObj = pattern.sub(r'<---->\1</---->' , line)  # \1...\9	匹配第n个分组的内容。
+        print serchObj
+        # print pattern.search(line).group()  # 对于一行中，包含多个类型的情况是否有相应的考虑
+        # serchList = pattern.findall(line)  # 对于一行中，包含多个类型的情况
+        # resList.extend(serchList)
+    # print resList
+
+    bold = re.compile(r'\*{2}(.*?)\*{2}')
+    text = 'Make this **cai**.  This **junsheng**.'
+    # print('Text:', text)
+    # print('Bold:', bold.sub(r'<b>\1</b>', text))
 
